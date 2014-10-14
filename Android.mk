66f4f05e00b57d47bedacfe77aaa44f8d54dfb56
forumber <kidobeko@gmail.com>
2014-10-14 18:34:39 +0300

    g2: add anc firmware symlink

--- Android.mk
+++ Android.mk
@@ -4,4 +4,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
+	ln -sf /data/misc/audio/wcd9320_anc.bin \
+	$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
+	ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
+	$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
+	ln -sf /data/misc/audio/mbhc.bin \
+	$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
+
 endif
