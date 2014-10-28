3e7a379ce3b5e5ad2aa27bccd644cab7f7bdbb85
forumber <kidobeko@gmail.com>
2014-10-28 16:07:37 +0200

    g2: Symlink firmware folder while installation

--- Android.mk
+++ Android.mk
@@ -18,4 +18,6 @@ $(shell ln -sf /system/etc/thermald-8974.conf \
 	ln -sf /system/etc/thermal-engine-8974.conf \
 	$(TARGET_OUT_ETC)/thermal-engine.conf)
 
+$(shell ln -sf /system/etc/firmware $(TARGET_OUT)/vendor/firmware)
+
 endif
