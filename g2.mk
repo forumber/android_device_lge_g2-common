1cf1fca25c64394a0398c7f7d5c35d312ae3d793
forumber <kidobeko@gmail.com>
2014-10-31 20:22:29 +0200

    g2: Switch to legacy alsa audio
    It should fix audio problems

--- g2.mk
+++ g2.mk
@@ -44,7 +44,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko
 
 # Thermal Configs
 PRODUCT_COPY_FILES += \
@@ -178,10 +179,10 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio_policy.msm8974 \
     libaudio-resampler \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors \
+#    libqcompostprocbundle \
+#    libqcomvisualizer \
+#    libqcomvoiceprocessing \
+#    libqcomvoiceprocessingdescriptors \
     tinymix
 
 # Omx
