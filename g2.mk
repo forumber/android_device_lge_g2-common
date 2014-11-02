e2447994b2cf92916fa7e978c3ebce2d83f90269
forumber <kidobeko@gmail.com>
2014-11-02 14:53:21 +0200

    g2: Tidy up

--- g2.mk
+++ g2.mk
@@ -237,19 +237,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	debug.composition.type=c2d \
 	debug.egl.recordable.rgba8888=1
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.texture_cache_size=72 \
-	ro.hwui.layer_cache_size=48 \
-	ro.hwui.r_buffer_cache_size=8 \
-	ro.hwui.path_cache_size=32 \
-	ro.hwui.gradient_cache_size=1 \
-	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.texture_cache_flushrate=0.4 \
-	ro.hwui.text_small_cache_width=1024 \
-	ro.hwui.text_small_cache_height=1024 \
-	ro.hwui.text_large_cache_width=2048 \
-	ro.hwui.text_large_cache_height=1024
-
 # QCOM
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
@@ -301,9 +288,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
-# QC Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.extension_library=/vendor/lib/libqc-opt.so
-
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
